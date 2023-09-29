c14af2fd1456b4dfb21aa2a850e7700e876679d7
capu <hi@capu.tech>
2023-09-29 11:43:59 -0300

    solve 18-magicnum using huff

--- test/18-MagicNum.t.sol
+++ test/18-MagicNum.t.sol
@@ -1,31 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+
 import {BaseTest} from "./BaseTest.sol";
 import {MagicNumFactory} from "../src/levels/18-MagicNumFactory.sol";
 import {MagicNum} from "../src/levels/18-MagicNum.sol";
+import "forge-std/console.sol";
 
 contract MagicNumSolution is BaseTest {
-    bytes19 constant private creationCode = 0x6009600a5f3960095ff3602a601f5360205ff3;
 
-    function solution(address payable target_) internal override{
+    function solution(address payable target_) internal override {
         MagicNum target = MagicNum(target_);
-        address deployment;
-        assembly {
-            // from the yul docs
-            function allocate(size) -> ptr {
-                ptr := mload(0x40)
-                if iszero(ptr) { ptr := 0x60 }
-                mstore(0x40, add(ptr, size))
-            }
-            let offset := allocate(19)
-            mstore(offset, creationCode)
-            deployment := create(0, offset, 19)
-        }
+
+        address deployment = HuffDeployer.deploy("magicnum");
+        console.logBytes(deployment.code);
         target.setSolver(deployment);
+        console.log(target.solver());
     }
 
-    function construction() internal override returns(address payable ) {
+    function construction() internal override returns (address payable) {
         return payable(address(new MagicNumFactory()));
     }
 }
